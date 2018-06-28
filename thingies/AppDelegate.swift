6e41714317357aea7a52b457fae15be008c37b87
MrLoh <flojoto@hotmail.com>
2018-06-28 19:43:07 +0200

    Initial Commit

---
+++ thingies/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  thingies
+//
+//  Created by Tobias Lohse on 28.6.2018.
+//  Copyright © 2018 Tobias Lohse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
