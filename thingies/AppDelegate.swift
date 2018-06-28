b50a0f071778fb7b6e326dcf6da4961aa99f00c8
MrLoh <flojoto@hotmail.com>
2018-06-28 21:12:54 +0200

    refactor into controller

--- thingies/AppDelegate.swift
+++ thingies/AppDelegate.swift
@@ -1,35 +1,8 @@
-//
-//  AppDelegate.swift
-//  thingies
-//
-//  Created by Tobias Lohse on 28.6.2018.
-//  Copyright © 2018 Tobias Lohse. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-	@IBOutlet weak var statusMenu: NSMenu!
-	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
-
-	@IBAction func quitClicked(_ sender: NSMenuItem) {
-		NSApplication.shared.terminate(self)
-	}
-
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		let icon = NSImage(named: NSImage.Name(rawValue: "statusIcon"))
-		icon?.isTemplate = true
-		statusItem.image = icon
-		statusItem.menu = statusMenu
-	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
-
-
+	func applicationDidFinishLaunching(_ aNotification: Notification) {}
+	func applicationWillTerminate(_ aNotification: Notification) {}
 }
 
