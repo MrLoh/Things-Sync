19aef20e901acd0c62a34b3dfb28663c3fc1e4d8
MrLoh <flojoto@hotmail.com>
2018-06-28 20:13:09 +0200

    setup statusbar app

--- thingies/AppDelegate.swift
+++ thingies/AppDelegate.swift
@@ -11,11 +11,17 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	@IBOutlet weak var window: NSWindow!
+	@IBOutlet weak var statusMenu: NSMenu!
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
+
+	@IBAction func quitClicked(_ sender: NSMenuItem) {
+		NSApplication.shared.terminate(self)
+	}
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		statusItem.title = "Thingies"
+		statusItem.menu = statusMenu
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
